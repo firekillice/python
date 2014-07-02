7e0d5cf53240b367cce168e40657a5fde637d8db
firekillice <firekillice@163.com>
2014-07-03 00:58:35 +0800

    update table

--- table2mongos/table.py
+++ table2mongos/table.py
@@ -1,11 +1,36 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
+import os
 
-class tablereader
-	filename_ = ''
+class tablereader:
+	__filename = ''
 
-	def __init__(self,file)
-		filename_ = file
+	def __init__(self,filename):
+		self.__filename = filename
+		self.__open__()
 
-	
+	def __open__(self):
+		print self.__filename
+		f = open(self.__filename)
+		lines = f.readlines()
+		f.close()
 		
+		print len(lines)
+		self.__content = [[]for i in range(len(lines))]
+		rdindex = 0
+		for line in lines:
+			line=line.strip('\n')			# remove "\n"
+			parts = line.split('\t')
+			for index in range(0,len(parts)):
+				self.__content[rdindex].append(parts[index])
+			rdindex += 1
+	
+	def get_content(self):
+		return self.__content
+
+if __name__ == "__main__":
+	_tablerder = tablereader('./equip_strengthen.csv')
+	_table = _tablerder.get_content()
+	for i in range(_table.__len__()):
+		for j in range(_table[i].__len__()):
+			print _table[i][j]
